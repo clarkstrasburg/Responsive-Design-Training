f8b6429643030e1f6db88a694f218e7369fb79cd
clarkstrasburg <34170564+clarkstrasburg@users.noreply.github.com>
2018-08-31 11:32:34 -0500

    Add files via upload

---
+++ RDTflex.js
@@ -0,0 +1,75 @@
+function openMenu() {
+    var element = document.getElementById("side-menu");
+    element.classList.toggle("menu-active");
+
+}
+
+function slideContent() {
+    var element = document.getElementById("site-body");
+    element.classList.toggle("site-body-active");
+}
+
+function dropdownNews() {
+    var element = document.getElementById("dropdown-news");
+    element.classList.toggle("dropdown-active-md");
+}
+
+function rotateNews() {
+    var element = document.getElementById("caret-news");
+    element.classList.toggle("dropdown-caret-active");
+}
+
+function dropdownAnnouncements() {
+    var element = document.getElementById("dropdown-announcements");
+    element.classList.toggle("dropdown-active-md");
+}
+
+function rotateAnnouncements() {
+    var element = document.getElementById("caret-announcements");
+    element.classList.toggle("dropdown-caret-active");
+}
+function dropdownSigned() {
+    var element = document.getElementById("content-block-five");
+    element.classList.toggle("dropdown-active-lg");
+}
+
+function rotateSigned() {
+    var element = document.getElementById("caret-signed");
+    element.classList.toggle("dropdown-caret-active");
+}
+function dropdownEvents() {
+    var element = document.getElementById("content-block-six");
+    element.classList.toggle("dropdown-active-lg");
+}
+
+function rotateEvents() {
+    var element = document.getElementById("caret-events");
+    element.classList.toggle("dropdown-caret-active");
+}
+function dropdownSidenav() {
+    var element = document.getElementById("side-menu-nav-links");
+    element.classList.toggle("side-menu-nav-active");
+}
+
+function rotateSidenav() {
+    var element = document.getElementById("caret-side-navigation");
+    element.classList.toggle("dropdown-caret-active");
+}
+function dropdownMylinks() {
+    var element = document.getElementById("side-menu-my-links");
+    element.classList.toggle("side-menu-mylinks-active");
+}
+
+function rotateMylinks() {
+    var element = document.getElementById("caret-my-links");
+    element.classList.toggle("dropdown-caret-active");
+}
+function dropdownShortcuts() {
+    var element = document.getElementById("side-menu-shortcuts");
+    element.classList.toggle("side-menu-shortcuts-active");
+}
+
+function rotateShortcuts() {
+    var element = document.getElementById("caret-shortcuts");
+    element.classList.toggle("dropdown-caret-active");
+}
